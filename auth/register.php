913de81d3337b3728f7bae674f52e052dd2e73d0
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-04-16 17:40:19 +0700

    add type of account

--- auth/register.php
+++ auth/register.php
@@ -31,22 +31,25 @@
                       $password = $_POST["password"];
                       $re_password = $_POST["re_password"];
                       $img = "web-coding.png";
-
+                      $type = $_POST["type"];
+                      
                       // check matching password and set password
                       if ($password == $re_password) {
-                        $insert = $conn->prepare("INSERT INTO users (username, email, password, img) VALUES (:username, :email, :password, :img) ");
+                        $insert = $conn->prepare("INSERT INTO users (username, email, password, img, type) VALUES (:username, :email, :password, :img, :type) ");
 
                         $insert->execute([
                           ":username" => $username,
                           ":email" => $email,
                           ":password" => password_hash($password, PASSWORD_DEFAULT),
-                          ":img" => $img
+                          ":img" => $img,
+                          ":type" => $type,
                         ]);
 
                         $register_success_alert = "<div class='alert alert-success bg-success text-white' role='alert'>User Successfully Registered</div>";
 
                         echo $register_success_alert;
-                        header("location: login.php");
+                      
+                        // header("location: login.php");
                       } else {
 
                         $register_failed_alert = "<div class='alert alert-danger bg-danger text-white' role='alert'>Password not match</div>";
@@ -70,6 +73,15 @@
                 <div class="col-md-12 mb-3 mb-md-0">
                   <label class="text-black" for="fname">Email</label>
                   <input type="text" id="fname" class="form-control" placeholder="Email address" name="email">
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label for="type" class="text-black">Select Type</label>
+                  <select class="selectpicker" data-style="btn-white btn-lg" data-width="100%" title="Select Type" name="type">
+                    <option>Job Suitors</option>
+                    <option>Company</option>
+                  </select>
                 </div>
               </div>
               <div class="row form-group">
