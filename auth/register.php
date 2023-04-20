abca269958199d5556197c71d7ab3d104a056529
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-04-20 16:28:11 +0700

    adding validation feature and username info in header

--- auth/register.php
+++ auth/register.php
@@ -1,10 +1,10 @@
-    <?php 
-      require '../config/config.php';
-      require '../include/header.php';
+    <?php
+    require "../config/config.php";
+    require "../include/header.php";
 
-      if(isset($_SESSION["username"])) {
-        header("location: ".APP_URL."");
-       } 
+    if (isset($_SESSION["username"])) {
+        header("location: " . APP_URL . "");
+    }
     ?>
     <!-- HOME -->
     <section class="section-hero overlay inner-page bg-image" style="background-image: url('../images/hero_1.jpg');" id="home-section">
@@ -25,44 +25,78 @@
       <div class="container">
         <div class="row">
 
-            <?php
-                if(isset($_POST["submit"])) {
-                  if(empty($_POST["username"]) OR empty($_POST["email"]) OR empty($_POST["re_password"])) {
+            <?php if (isset($_POST["submit"])) {
+                if (
+                    empty($_POST["username"]) or
+                    empty($_POST["email"]) or
+                    empty($_POST["re_password"] or empty($_POST["type"]))
+                ) {
                     echo '<div class="alert bg-danger text-white alert-danger" role="alert">Some input are empty</div>';
-                  } else {
-                      $username = $_POST["username"];
-                      $email = $_POST["email"];
-                      $password = $_POST["password"];
-                      $re_password = $_POST["re_password"];
-                      $img = "web-coding.png";
-                      $type = $_POST["type"];
-                      
-                      // check matching password and set password
-                      if ($password == $re_password) {
-                        $insert = $conn->prepare("INSERT INTO users (username, email, password, img, type) VALUES (:username, :email, :password, :img, :type) ");
+                } else {
+                    $username = $_POST["username"];
+                    $email = $_POST["email"];
+                    $password = $_POST["password"];
+                    $re_password = $_POST["re_password"];
+                    $img = "web-coding.png";
+                    $type = $_POST["type"];
 
-                        $insert->execute([
-                          ":username" => $username,
-                          ":email" => $email,
-                          ":password" => password_hash($password, PASSWORD_DEFAULT),
-                          ":img" => $img,
-                          ":type" => $type,
-                        ]);
+                    // checking length of character
+                    if (strlen($email) > 25 OR strlen($username) > 25) {
+                        $input_error =
+                            '<p class="text-danger">Text is too length</p>';
+                    } else {
+                        $input_error = "";
+                    }
 
-                        $register_success_alert = "<div class='alert alert-success bg-success text-white' role='alert'>User Successfully Registered</div>";
+                    // check matching password and set password
+                    if ($password == $re_password) {
+                        if ($input_error == "") {
+                            // check if email and username is already registered
+                            $check_user = $conn->prepare("SELECT * FROM users WHERE email = :email OR username = :username");
 
-                        echo $register_success_alert;
-                      
-                        // header("location: login.php");
-                      } else {
+                            $check_user->execute([
+                              ":email" => $email,
+                              ":username" => $username,
+                            ]);
 
-                        $register_failed_alert = "<div class='alert alert-danger bg-danger text-white' role='alert'>Password not match</div>";
+                            if($check_user->rowCount() > 0) {
+                              $register_failed_alert = "<div class='alert alert-danger bg-danger text-white' role='alert'>Email or Username has already exist</div>";
+                              echo $register_failed_alert;
+                            } else {
+                                // proceed if username is valid
+                                $insert = $conn->prepare(
+                                    "INSERT INTO users (username, email, password, img, type) VALUES (:username, :email, :password, :img, :type) "
+                                );
 
+                                $insert->execute([
+                                    ":username" => $username,
+                                    ":email" => $email,
+                                    ":password" => password_hash(
+                                        $password,
+                                        PASSWORD_DEFAULT
+                                    ),
+                                    ":img" => $img,
+                                    ":type" => $type,
+                                ]);
+
+                                $direct_to_login_page = APP_URL."/auth/login.php";
+
+                                $register_success_alert = "<div class='alert alert-success bg-success text-white' role='alert'>User Successfully Registered <a href='{$direct_to_login_page}' class='text-white'> Login Here</a> </div>";
+                                 
+                                echo $register_success_alert;
+                            }
+                        } else {
+                            $register_failed_alert =
+                                "<div class='alert alert-danger bg-danger text-white' role='alert'>". $email_error ."</div>";
+                            echo $register_failed_alert;
+                        }
+                    } else {
+                        $register_failed_alert =
+                            "<div class='alert alert-danger bg-danger text-white' role='alert'>Password not match</div>";
                         echo $register_failed_alert;
-                      }
-                  }
+                    }
                 }
-               ?>
+            } ?>
 
           <div class="col-md-12 mb-5">
             <form action="register.php" method="POST" class="p-4 border rounded">
@@ -76,13 +110,18 @@
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
                   <label class="text-black" for="fname">Email</label>
-                  <input type="text" id="fname" class="form-control" placeholder="Email address" name="email">
+                  <input type="email" id="fname" class="form-control" placeholder="Email address" name="email">
+                  <?php 
+                    if(isset($input_error)) {
+                      echo $input_error;  
+                    }
+                  ?>
                 </div>
               </div>
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
                   <label for="type" class="text-black">Select Type</label>
-                  <select class="selectpicker" data-style="btn-white btn-lg" data-width="100%" title="Select Type" name="type">
+                  <select class="selectpicker border" data-style="btn-white btn-lg" data-width="100%" title="Select Type" name="type">
                     <option>Job Suitors</option>
                     <option>Company</option>
                   </select>
@@ -113,4 +152,4 @@
         </div>
       </div>
     </section>
-    <?php require '../include/footer.php'; ?>
+    <?php require "../include/footer.php"; ?>
--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>JobBoard &mdash; Website Template by Colorlib</title>
+    <title>JobBoard &mdash; Find your favorite job</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="" />
